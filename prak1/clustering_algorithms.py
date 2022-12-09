392927cee641bcd120c7aa8bd30600b6e71b7a76
Clemens <clemensdittmann@outlook.com>
2022-12-09 22:10:05 +0100

    setup

---
+++ prak1/clustering_algorithms.py
@@ -0,0 +1,11 @@
+
+def kmeans(vectors: list, k=10) -> list:
+    # todo: implement k-means here!
+
+    return None
+
+
+def dbscan(vectors: list, minpts: int, epsilon: int) -> list:
+    # todo: implement k-means here!
+
+    return None
