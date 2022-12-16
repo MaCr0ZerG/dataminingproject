48a2aced269d282a77f37c07c83f3ac36e000116
Niklas <niklasarlt@gmail.com>
2022-12-16 23:36:18 +0100

    dbscan first implementation

--- prak1/clustering_algorithms.py
+++ prak1/clustering_algorithms.py
@@ -1,3 +1,5 @@
+import math
+
 
 def kmeans(vectors: list, k=10) -> list:
     # todo: implement k-means here!
@@ -5,7 +7,55 @@ def kmeans(vectors: list, k=10) -> list:
     return None
 
 
-def dbscan(vectors: list, minpts: int, epsilon: int) -> list:
-    # todo: implement k-means here!
+def dbscan(vectors: list, minpts: int, epsilon: float) -> list:
+
+    # todo: implement dbscan here!
+    clusters = list(list(Vector))
+    for i in vectors:
+        tempvectors = list(Vector)
+        tempvectors[i] = Vector(i,-1,neighbours(i,vectors,epsilon))
+    id = 0
+    for i in tempvectors:
+        if i.id == -1:
+            if i.neighbours.len < minpts:
+                 i.id = 0
+            else:
+                id += 1
+                seeds = neighbours
+                for j in seeds:
+                    j.id = id
+                seeds.remove(i)
+                while seeds.len != 0:
+                    k = seeds[0]
+                    if k.neighbours >= minpts:
+                        for l in k.neighbours:
+                            if l.id < 1:
+                                if l.id == -1:
+                                    seeds.append(l)
+                                l.id = id
+                    seeds.remove(k)                                
 
     return None
+
+def neighbours(vector = list,vectors = list, epsilon = float) -> list:
+    neighbours = list
+    for i in vectors:
+        if distance(vector, i) <= epsilon:
+            neighbours.append(i)
+    return neighbours
+
+def distance(vector1: list, vector2: list ) -> float:
+
+    # distance between two n-dimensional points.
+    distance = float
+    for i in vector1.count:
+        distance += (vector1[i] - vector2[i])^2
+    
+    distance = math.sqrt(distance)
+    return distance
+
+class Vector :
+    def __init__(self, list, id, neighbours):
+        self.list = list
+        self.id = id
+        self.neighbours = neighbours
